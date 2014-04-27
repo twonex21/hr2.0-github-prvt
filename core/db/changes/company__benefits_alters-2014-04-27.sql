849166443b4e63f832359500ec76138d62bb13a2
armenmargaryan <armen.margarian@gmail.com>
2014-04-27 15:27:27 +0400

    db changes for coompany section

---
+++ core/db/changes/company__benefits_alters-2014-04-27.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `hr_benefits` CHANGE `id` `benefit_id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+
+ALTER TABLE `hr_company_benefits` CHANGE `id` `company_benefits_id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
