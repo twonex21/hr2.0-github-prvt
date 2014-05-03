9debf9305e0902b610a24a9cb780f4a2ec4b3712
armenmargaryan <armen.margarian@gmail.com>
2014-05-03 20:55:00 +0400

    addeding .sql file correct content

--- core/db/changes/20140503_hr_company_subscription_create.sql
+++ core/db/changes/20140503_hr_company_subscription_create.sql
@@ -1,1 +1,5 @@
-ALTER TABLE `hr_vacancy_experience` DROP COLUMN `id`, DROP PRIMARY KEY;
+CREATE TABLE IF NOT EXISTS `hr_company_subscription` (
+  `company_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `changed_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
